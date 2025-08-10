ba59a781e980d451c261681b004e4cc551659f47
hung hsiang Chien <kevinchien@hungdeMacBook-Air.local>
2025-08-10 22:03:54 +0800

    remove unnecessary import

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -1,6 +1,5 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
-import { Photos } from "./quartz/components/Photos"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
